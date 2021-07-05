a4f211474d0bb94308630c15e91e07f96e74148c
THARUN.V <v.tharuniitm001@gmail.com>
2021-07-05 14:07:46 +0530

    ADD

---
+++ form.js
@@ -0,0 +1,20 @@
+function passwor() {
+    let firstName = document.getElementById("Firstname").value;
+    let lastName = document.getElementById("lastname").value;
+    let password = document.getElementById("password").value;
+    if (password === firstName + "@" + lastName) {
+        alert("SUCCESS");
+    }
+    else {
+        alert("ERROR");
+    }
+}
+function chec() {
+    let check = document.getElementById("checkbox");
+    if (check.checked === true) {
+        document.getElementById("submit").disabled = false;
+    }
+    else {
+        document.getElementById("submit").disabled = true;
+    }
+}
